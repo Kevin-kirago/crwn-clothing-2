ea121ecc0bb6e4843fdf45b5be3103437f677a8e
Kevin <tkevinkiragu@gmail.com>
2021-12-16 12:41:07 +0300

    added routing with react-router-dom @5.1.1 and updated our menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/home/homepage.component";
 
-function App() {
+const HatsPage = (props) => {
+	console.log(props);
 	return (
-		<div className="App">
-			<HomePage />
+		<div>
+			<h1>Hats Page</h1>
+			<Link to="/shop">HomePage</Link>
 		</div>
 	);
-}
+};
+
+const App = () => {
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/shop" component={HomePage} />
+				<Route exact path="/shop/hats" component={HatsPage} />
+			</Switch>
+		</div>
+	);
+};
 
 export default App;
